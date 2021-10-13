bbaabcf2dbbb436f554030400cfd063517c772db
Abdulla-Khan <abdullahkhan.smiu@gmail.com>
2021-10-13 19:58:36 +0100

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:app_shoe_shop/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/models/shoe_models.dart
@@ -0,0 +1,40 @@
+import 'package:app_shoe_shop/core/const.dart';
+import 'package:flutter/cupertino.dart';
+
+class ShoeModel {
+  final String name;
+  final double price;
+  final String decs;
+  final Color color;
+  final String brand;
+  final String imgPath;
+
+  ShoeModel(
+      {this.name, this.price, this.decs, this.color, this.brand, this.imgPath});
+  static List<ShoeModel> list = [
+    ShoeModel(
+        name: "Nike Air Jordan 1 Retro High",
+        decs:
+            "Familiar but always Fresh, the iconic Air jordan 1 is remastered for today's sneakerhead culture. This Retro High OG version goes all in with full-grain leather, comfortable cushioning and classic design details.",
+        price: 184,
+        color: AppColors.blueColor,
+        brand: "Nike",
+        imgPath: "2.png"),
+    ShoeModel(
+        name: "Converse X OPI Chuck Taylor",
+        decs:
+            "Familiar but always Fresh, the iconic Air jordan 1 is remastered for today's sneakerhead culture. This Retro High OG version goes all in with full-grain leather, comfortable cushioning and classic design details.",
+        price: 184,
+        color: AppColors.yellowColor,
+        brand: "Converse",
+        imgPath: "1.png"),
+    ShoeModel(
+        name: "Nike Air Jordan 1 Retro High",
+        decs:
+            "Familiar but always Fresh, the iconic Air jordan 1 is remastered for today's sneakerhead culture. This Retro High OG version goes all in with full-grain leather, comfortable cushioning and classic design details.",
+        price: 184,
+        color: AppColors.redColor,
+        brand: "Nike",
+        imgPath: "3.png")
+  ];
+}
